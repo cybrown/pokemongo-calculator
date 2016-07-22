ec2e4407a806fca3cfcf768702ee53df2f54062d
Florian Vichery <florian.vichery@gmail.com>
2016-07-22 21:31:20 +0200

    lucky egg optimizer v1

--- app/lucky-egg/lucky-egg-controller.ts
+++ app/lucky-egg/lucky-egg-controller.ts
@@ -1,13 +1,63 @@
-import {LuckyEggService} from "./lucky-egg-service";
-import {IPokemon} from "./lucky-egg-service";
+import {LuckyEggService, IPokemon, IBagData, IBagOptimization} from "./lucky-egg-service";
 
 export class LuckyEggController {
 
+    pokemonNameLang: string;
+
     pokemonList: IPokemon[];
+
+    bagDataList: IBagData[];
+
+    bagInput: IBagData;
+
+    bagOptimization: IBagOptimization;
 
     static $inject = ["LuckyEggService"];
 
     constructor(private luckyEggService: LuckyEggService) {
         this.pokemonList = luckyEggService.constructPokemonList();
+        this.bagDataList = localStorage.getItem("data") ? JSON.parse(localStorage.getItem("data")) : [];
+        this.pokemonNameLang = localStorage.getItem("lang") ? localStorage.getItem("lang") : "En";
+        this.initInput();
+    }
+
+    addInputToBag() {
+        this.bagDataList.push(this.bagInput);
+        this.initInput();
+    }
+
+    removeFromBag(data: IBagData) {
+        this.bagDataList = this.bagDataList.filter((value: IBagData) => value !== data);
+    }
+
+    initInput() {
+        this.bagInput = {
+            pokemon: null,
+            pokemonCount: null,
+            candyCount: null
+        };
+    }
+
+    optimizeBag() {
+        localStorage.setItem("data", JSON.stringify(this.bagDataList));
+        this.bagOptimization = this.luckyEggService.optimize(this.bagDataList);
+    }
+
+    resetData() {
+        this.bagDataList = [];
+        localStorage.removeItem("data");
+    }
+
+    setPokemonNameLang(lang: string) {
+        localStorage.setItem("lang", lang);
+        this.pokemonNameLang = lang;
+    }
+
+    getPokemonName(pokemon: IPokemon): string {
+        if (this.pokemonNameLang === "Fr") {
+            return pokemon.nameFr;
+        } else {
+            return pokemon.nameEn;
+        }
     }
 }
--- app/lucky-egg/lucky-egg-service.ts
+++ app/lucky-egg/lucky-egg-service.ts
@@ -7,24 +7,85 @@ export interface IPokemon {
 
 export interface IBagData {
     pokemon: IPokemon;
-    pokemonnCount: number;
+    pokemonCount: number;
     candyCount: number;
+}
+
+export interface IBagOptimization {
+    optimizations: IPokemonOptimization[];
+    timeToEvolve: number;
+    totalEvolutionCount: number;
+}
+
+export interface IPokemonOptimization {
+    pokemon: IPokemon;
+    transferCount: number;
+    evolutionCount: number;
+    pokemonLeft: number;
+    candyLeft: number;
 }
 
 export class LuckyEggService {
 
     static $inject = [];
 
+    static TIME_TO_EVOLVE = 0.5;
+    static XP_PER_EVOLVE = 500;
+
     constructor() {
         // no default action
     }
-
-
 
     public constructPokemonList(): IPokemon[] {
         let data = <any> require("json!../data/pokemon-data.json");
         return data;
     }
 
+    public optimize(bag: IBagData[]): IBagOptimization {
 
+        let bagOptimization: IBagOptimization = {
+            optimizations: [],
+            timeToEvolve: 0,
+            totalEvolutionCount: 0
+        };
+
+        bag.forEach(bagData => {
+            let pokemonOptimization: IPokemonOptimization = null;
+            let possibleEvolutionCount = Math.floor(bagData.candyCount / bagData.pokemon.candiesToEvolve);
+            if (possibleEvolutionCount > bagData.pokemonCount) {
+                pokemonOptimization = {
+                    pokemon: bagData.pokemon,
+                    transferCount: 0,
+                    evolutionCount: bagData.pokemonCount,
+                    pokemonLeft: 0,
+                    candyLeft: bagData.candyCount - bagData.pokemonCount * bagData.pokemon.candiesToEvolve
+                };
+            } else {
+                let candiesLeftAfterFirstEvolution = bagData.candyCount % bagData.pokemon.candiesToEvolve;
+                let possibleTransferCount = bagData.pokemonCount - possibleEvolutionCount;
+
+                let supplementEvolutionCount = Math.floor((possibleTransferCount + candiesLeftAfterFirstEvolution) / (bagData.pokemon.candiesToEvolve + 1));
+
+                let transferCount = Math.max(supplementEvolutionCount * bagData.pokemon.candiesToEvolve - candiesLeftAfterFirstEvolution, 0);
+                let evolutionCount = possibleEvolutionCount + supplementEvolutionCount;
+
+                let pokemonLeft = bagData.pokemonCount - transferCount - evolutionCount;
+                let candyLeft = bagData.candyCount + transferCount - (bagData.pokemon.candiesToEvolve * evolutionCount);
+
+                pokemonOptimization = {
+                    pokemon: bagData.pokemon,
+                    transferCount: transferCount,
+                    evolutionCount: evolutionCount,
+                    pokemonLeft: pokemonLeft,
+                    candyLeft: candyLeft
+                };
+            }
+
+            bagOptimization.optimizations.push(pokemonOptimization);
+            bagOptimization.timeToEvolve += pokemonOptimization.evolutionCount * LuckyEggService.TIME_TO_EVOLVE;
+            bagOptimization.totalEvolutionCount += pokemonOptimization.evolutionCount;
+        });
+
+        return bagOptimization;
+    }
 }
